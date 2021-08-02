5d4d31e0526c1fd6c22f4a97e36880772564296b
Gabriel Deleon <gabrieldeleon@Gabriels-MacBook-Pro.local>
2021-08-02 15:55:40 -0400

    sign in, sing out, home views complete

---
+++ client/src/views/SignIn/SignIn.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import { signIn } from '../../services/users'
+import Layout from "../../components/Layout/Layout"
+import { useHistory } from 'react-router-dom'
+
+export default function SignIn(props) {
+    const [input, setInput] = useState({ email: "", password: ""})
+    const {setUser} = props
+    const history = useHistory();
+
+
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        const user = await signIn(input)
+        console.log(user)
+        setUser(user)
+        history.push('/')
+    }
+
+    const handleInput = (e) => {
+        const {id, value } = e.target;
+        setInput((prevInput) => ({
+            ...prevInput,
+            [id]: value,
+        }))
+    }
+
+    return (
+        <Layout>
+            <br />
+            Sign In
+            <br />
+            <br />
+            <form onSubmit={handleSubmit}>
+                <label>Email</label>
+                <br />
+                <input 
+                    type="email"
+                    id="email"
+                    value={input.email}
+                    onChange={handleInput}
+                />
+                <br />
+                <label>Password</label>
+                <br />
+                <input 
+                    type="password"
+                    id="password"
+                    value={input.password}
+                    onChange={handleInput}
+                />
+                <br />
+                <button>Sign In</button>
+                <br />
+            </form>
+        </Layout>
+    )
+}
---
+++ client/src/views/SignUp/SignUp.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import { signUp } from '../../services/users'
+import Layout from "../../components/Layout/Layout.jsx"
+import { useHistory } from 'react-router-dom'
+
+export default function SignUp(props) {
+    const [input, setInput] = useState({ username: "", email: "", password: ""})
+    const history = useHistory();
+
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        const user = await signUp(input)
+        console.log(user)
+        props.setUser(user)
+        history.push('/')
+    }
+
+    const handleInput = (e) => {
+        const {id, value } = e.target;
+        setInput((prevInput) => ({
+            ...prevInput,
+            [id]: value,
+        }))
+    }
+
+    return (
+        <Layout>
+            <br />
+            Sign Up
+            <br />
+            <br />
+            <form onSubmit={handleSubmit}>
+            <label>Username</label>
+                <br />
+                <input
+                type="text"
+                id="username"
+                value={input.username}
+                onChange={handleInput}
+                />
+                <br />
+                <label>Email</label>
+                <br />
+                <input 
+                    type="email"
+                    id="email"
+                    value={input.email}
+                    onChange={handleInput}
+                />
+                <br />
+                <label>Password</label>
+                <br />
+                <input 
+                    type="password"
+                    id="password"
+                    value={input.password}
+                    onChange={handleInput}
+                />
+                <br />
+                <button>Sign Up</button>
+                <br />
+            </form>
+        </Layout>
+    )
+}
