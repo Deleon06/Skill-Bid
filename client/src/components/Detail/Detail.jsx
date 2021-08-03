3f6c444ee6e31dd5012b7dd0583f4a61a31a1825
Cameron Izadi <cameronizadi@Camerons-MBP.fios-router.home>
2021-08-03 17:06:29 -0400

    Revised Detail with Form

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import Layout from "../../components/Layout/Layout"
+import { useState } from "react"
+import { useHistory } from "react-router"
 
 export default function Detail() {
-    return (
+  const [input, setInput] = useState("");
+
+  const history = useHistory();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Your message was sent!")
+    alert("Your message was successfully sent!")
+    history.push("/")
+  }
+  
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setInput((prevInput) => ({
+      ...prevInput,
+      [id]: value,
+    }))
+  }
+
+  return (
+    <Layout>
+        <div id="container">
         <div className="detail-container">
         {jobs.map((job) => (
             <div>
@@ -12,5 +36,40 @@ export default function Detail() {
             </div>
             ))}
         </div>
+        
+      <div id="form">
+        <label>Contact Seller</label>
+          <form onSubmit={handleSubmit}>
+            <label>Name</label>
+            <input
+              type="text"
+              id="name"
+              placeholder="Name"
+              value={input.name}
+              onChange={handleChange}
+            />
+            <br />
+            <label>Email</label>
+            <input
+              type="text"
+              id="email"
+              value={input.email}
+              onChange={handleChange}
+            />
+            <br />
+
+            <label>Description</label>
+            <input
+              type="text"
+              id="description"
+              value={input.description}
+            onChange={handleChange}
+            />
+              <br />
+            <button>Send</button>
+        </form>
+      </div>
+      </div>
+      </Layout>
     )
 }
