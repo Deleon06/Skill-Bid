a0fc82c7784d074ca86edcdc9721ac57d87755c6
Gabriel Deleon <gabrieldeleon@gabriels-mbp.myfiosgateway.com>
2021-08-03 10:20:26 -0400

    adding changes

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -10,31 +10,31 @@ export default function Footer() {
             <h5 className="created">Created By:</h5>
             <p>Ryan Lee</p> 
             <a rel="noreferrer" href="https://github.com/AleksandrGorbach" target="_blank">
-            <FontAwesomeIcon icon={FaGitHub} id="git" />
+            <FontAwesomeIcon icon={faGithub} id="git" />
             </a>
             <a rel="noreferrer" href="https://www.linkedin.com/in/aleksandr-gorbach-85a314154/" target="_blank">
-            <FontAwesomeIcon icon={FaLinkedin} id="link" />
+            <FontAwesomeIcon icon={faLinkedin} id="link" />
             </a>
             <p>Cameron Izadi</p> 
             <a rel="noreferrer" href="https://github.com/AleksandrGorbach" target="_blank">
-            <FontAwesomeIcon icon={FaGitHub} id="git" />
+            <FontAwesomeIcon icon={faGithub} id="git" />
             </a>
             <a rel="noreferrer" href="https://www.linkedin.com/in/aleksandr-gorbach-85a314154/" target="_blank">
-            <FontAwesomeIcon icon={FaLinkedin} id="link" />
+            <FontAwesomeIcon icon={faLinkedin} id="link" />
             </a>
             <p>Gabriel Deleon</p> 
             <a rel="noreferrer" href="https://github.com/AleksandrGorbach" target="_blank">
-            <FontAwesomeIcon icon={FaGitHub} id="git" />
+            <FontAwesomeIcon icon={faGithub} id="git" />
             </a>
             <a rel="noreferrer" href="https://www.linkedin.com/in/aleksandr-gorbach-85a314154/" target="_blank">
-            <FontAwesomeIcon icon={FaLinkedin} id="link" />
+            <FontAwesomeIcon icon={faLinkedin} id="link" />
             </a>
             <p>Aleksandr Gorbach</p> 
             <a rel="noreferrer" href="https://github.com/AleksandrGorbach" target="_blank">
-            <FontAwesomeIcon icon={FaGitHub} id="git" />
+            <FontAwesomeIcon icon={faGithub} id="git" />
             </a>
             <a rel="noreferrer" href="https://www.linkedin.com/in/aleksandr-gorbach-85a314154/" target="_blank">
-            <FontAwesomeIcon icon={FaLinkedin} id="link" />
+            <FontAwesomeIcon icon={faLinkedin} id="link" />
             </a>
         </div>
     )
