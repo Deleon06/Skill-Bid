15288afd88e7292af770ae30ae055d728f780de4
Sang Hyun Lee <you@example.com>
2021-08-03 15:59:20 -0400

    contactUs update

--- client/src/views/ContactUs/ContactUs.jsx
+++ client/src/views/ContactUs/ContactUs.jsx
@@ -1,17 +1,47 @@
-import React from 'react'
-import {useState} from 'react'
+import { useState } from 'react';
+import Layout from '../../components/Layout/Layout';
+import { useHistory } from 'react-router';
 
 export default function ContactUs() {
-    const [input, setInput] 
-    return (
+  const [input, setInput] = useState({});
+  const history = useHistory();
+
+  const handleChange = (e) => {
+    const {id, value} = e.target;
+    setInput((prevInput) => ({
+        ...prevInput,
+        [id]: value,
+    }))
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log('Your message was sent!')
+    alert("Your message was successfully sent!")
+    history.push('/')
+}
+
+  return (
+      <Layout>
         <div>
-            <form className="aboutUs">
-                <label>Name</label>
+            <form className="contactUs" onSubmit={handleSubmit}>
                 <input 
-                    type="text"
-                    placeholder="name"
-                />
+            type="text"
+            id="name"
+            placeholder="Name"
+            value={input.name}
+            onChange={handleChange}
+          /><br />
+          <textarea 
+            type="text"
+            id="message"
+            placeholder="Message"
+            value={input.message}
+            onChange={handleChange}
+          /><br />
+          <button>Contact Us</button>
             </form>
-        </div>
+      </div>
+      </Layout>
     )
 }
