a5e153243722655b0ba3a02b81135af7bbc24acc
aleksandr gorbach <alex@aleksandrs-Air.fios-router.home>
2021-08-03 10:09:06 -0400

    depend for aos

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -16,10 +16,11 @@ const Navbar = () => {
                 <span />
             </Hamburger>
             <Menu isOpen={isOpen}>
-                <MenuLink href="/"></MenuLink>
-                <MenuLink href=""></MenuLink>
-                <MenuLink href=""></MenuLink>
-                <MenuLink href=""></MenuLink>
+                <MenuLink href="/">Home</MenuLink>
+                <MenuLink href="/New Post">New Post</MenuLink>
+                <MenuLink href="/Conatact">Contact</MenuLink>
+                <About />
+                <MenuLink href="Sign-In">Sign-In</MenuLink>
                 <About />
             </Menu>
         </Nav>
@@ -38,7 +39,7 @@ const Nav = styled.div`
 const Hamburger = styled.div`
     display: none;
     flex-direction: column;
-    curser: pointer;
+    cursor: pointer;
 
     span{
         height: 2px;
