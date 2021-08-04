7acd4d716286442f2bfa9ea8342b574859d931b0
Gabriel Deleon <gabrieldeleon@Gabriels-MacBook-Pro.local>
2021-08-04 15:36:14 -0400

    sign in

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "./Card.css";
-import {getAllJobs} from "../../services/jobs"
+import {getAllJobs, getBathroom, getElectrical, getFloor, getKitchen, getLandscape, getPlumbing, getRoof, getWall, getCeiling, getPaint} from "../../services/jobs"
 import {useState, useEffect} from "react"
 
 AOS.init();
@@ -15,23 +15,96 @@ export default function Card(props) {
             let data = await getAllJobs()
             setJobs(data)
         }
-    })
-    //     const fetchFilteredTasks = async () {
-    //         let data = await `getAll${props}`
-    //     }
-    //     fetchTasks()
-    // }, [])
+        fetchTasks();
+    
+    
+        if (props.value === "Bathroom") {
+            const fetchTasks = async () => {
+                let data = await getBathroom()
+                setJobs(data)
+            }
+            fetchTasks();
+        }
 
-    // const displayEditLink = (job) => {
-    //     if (job.userId === props.user?.id) {
-    //         return <Link to={`/edit-job/${job._id}`}>Edit</Link>
-    //     }
-    // }
+        if (props.value === "Ceiling") {
+            const fetchTasks = async () => {
+                let data = await getCeiling()
+                setJobs(data)
+            }
+            fetchTasks();
+        }
+
+        if (props === "Electrical") {
+            const fetchTasks = async () => {
+                let data = await getElectrical()
+                setJobs(data)
+            }
+            fetchTasks();
+        }
+
+        if (props === "Floor") {
+            const fetchTasks = async () => {
+                let data = await getFloor()
+                setJobs(data)
+            }
+            fetchTasks();
+        }
+
+        if (props === "Kitchen") {
+            const fetchTasks = async () => {
+                let data = await getKitchen()
+                setJobs(data)
+            }
+            fetchTasks();
+        }
+
+        if (props === "Landscape") {
+            const fetchTasks = async () => {
+                let data = await getLandscape()
+                setJobs(data)
+            }
+            fetchTasks();
+        }
+
+        if (props === "Paint") {
+            const fetchTasks = async () => {
+                let data = await getPaint()
+                setJobs(data)
+            }
+            fetchTasks();
+        }
+
+        if (props === "Plumbing") {
+            const fetchTasks = async () => {
+                let data = await getPlumbing()
+                setJobs(data)
+            }
+            fetchTasks();
+        }
+
+        if (props === "Roof") {
+            const fetchTasks = async () => {
+                let data = await getRoof()
+                setJobs(data)
+            }
+            fetchTasks();
+        }
+
+        if (props === "Wall") {
+            const fetchTasks = async () => {
+                let data = await getWall()
+                setJobs(data)
+            }
+            fetchTasks();
+        }
+
+    },[])
+
     return (
         
         <>
         {jobs.map((job) => (
-            <div data-aos="fade-up" className="card-container" key={job._id}>
+            <div data-aos="fade-up" className="card-container" id={job.projectType} key={job._id}>
                 <div className="job">
                     Name of person: {job.name}
                 </div>
