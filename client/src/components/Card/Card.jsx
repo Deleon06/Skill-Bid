edca06057da225d357e6b78d4c8edfb446b163f8
Cameron Izadi <cameronizadi@Camerons-MBP.fios-router.home>
2021-08-09 10:24:53 -0400

    Slicing Changes

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -20,8 +20,10 @@ const [toggle, setToggle] = useState(false)
         const fetchTasks = async () => {
             if (props.value === undefined || props.value.projectType === "All"){
               let data = await getAllJobs()
+
               // let copyData = [...data]
-              // countSplice(copyData, count, count+10, page, "up")
+              // countSplice(copyData, count, count + 10, page, "up")
+              
               console.log(data)
                 setJobs(data)
             }
@@ -48,12 +50,48 @@ const [toggle, setToggle] = useState(false)
       document.location.href="/"
     }
   }
-
+  // const handleUp = (e) => {
+  //     if (count > copyData.length) {
+  //       count = 0;
+  //       page = 0;
+  //     } else {
+  //       countSplice(copyData, count, count + 10, page, "up")
+  //     }
+  // }
   
+  // const handleDown = (e) => {
+  //     if (count < 0) {
+  //       count = 0;
+  //       page = 0;
+  //     } else {
+  //       countSplice(copyData, count, count + 10, page, "down")
+  //     }
+  //   }
+
+  //   function countSplice(array, begin, end, page, direction) {
+  //     console.log(count)
+    
+  //     function helper(array, begin, end){
+  //       let newArr = array.slice(begin, end)
+  //       console.log(newArr)
+  //     }  
+  //     if (direction === "up") {
+  //       helper(array, begin, end)
+  //       count += 10;
+  //       page += 1;
+        
+  //     } else if (direction === "down") {
+  //       helper(array, begin - 20, end - 20)
+  //       count -= 10;
+  //       page -= 1;
+  //     }
+  //   }
 
     return (
         
       <>
+        {/* <button className="down" onClick={handleDown} > ← </button>
+        <button className="up" onClick={handleUp} > → </button> */}
         {jobs.map((job) => (
           <div data-aos="zoom-in-up" data-aos-duration="1000" className="card-container" id={job.projectType} key={job._id}>
             <Link to={`/post/${job._id}`} key={job._id}> 
