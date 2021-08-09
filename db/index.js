b93943666c6c0b0581b1da388922e21a0cf8c331
aleksandr gorbach <alex@aleksandrs-Air.fios-router.home>
2021-08-09 19:21:19 -0400

    whitelist

--- db/index.js
+++ db/index.js
@@ -2,7 +2,20 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI =
   process.env.PROD_MONGODB || "mongodb://localhost:27017/skillBidDatabase";
-  
+
+const whitelist = ["https://6111b6e0f39f0b000782f529--awesome-jepsen-f2cb4f.netlify.app/sign-up“];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(
+                new Error("Not allowed by CORS; origin domain needs to be added to whitelist.")
+            );
+        }
+    },
+};
+
 mongoose.set("debug", true);
   
 mongoose.set("useCreateIndex", true);
