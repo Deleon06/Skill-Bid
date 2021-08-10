e3177bca3bf51cdbe9683e599a4ab31bf407dab4
aleksandr gorbach <alex@aleksandrs-Air.fios-router.home>
2021-08-10 17:13:10 -0400

    f

--- server.js
+++ server.js
@@ -8,21 +8,21 @@ import routes from "./routes/index.js"
 const app = express();
 const PORT = process.env.PORT || 4567;
 
-const whitelist = ["https://awesome-jepsen-f2cb4f.netlify.app/*"];
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(
-                new Error("Not allowed by CORS; origin domain needs to be added to whitelist.")
-            );
-        }
-    },
-};
+// const whitelist = ["https://awesome-jepsen-f2cb4f.netlify.app/*"];
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         if (whitelist.indexOf(origin) !== -1) {
+//             callback(null, true);
+//         } else {
+//             callback(
+//                 new Error("Not allowed by CORS; origin domain needs to be added to whitelist.")
+//             );
+//         }
+//     },
+// };
 
 app.use(express.json());
-app.use(cors(corsOptions)); 
+app.use(cors()); 
 app.use(morgan("dev"));
 
 app.use("/api", routes)
