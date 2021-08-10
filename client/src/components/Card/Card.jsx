d2494086cc6df739d5eb98da95246dc4156d7995
Gabriel Deleon <gabrieldeleon@Gabriels-MacBook-Pro.local>
2021-08-10 09:39:51 -0400

    cards

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -31,6 +31,7 @@ export default function Card(props) {
             }
         }
         fetchTasks();
+        console.log(jobs)
     },[props]) 
     
     async function handleDelete(e) {
@@ -40,19 +41,22 @@ export default function Card(props) {
     
     const handleSubmit = async (e) => {
         e.preventDefault()
+        console.log(props.user.id)
+        console.log(e.target.parentElement.accessKey)
         if(props.user.id === e.target.parentElement.accessKey) {
-         (window.confirm("Are you sure you want to delete the job post?")) 
+         if(window.confirm("Are you sure you want to delete the job post?")) {
         handleDelete(e)
         history.push("/")
         document.location.href="/"
         }
+    }
     }
 
     return (
         
         <>
         {jobs.map((job) => (
-            <div data-aos="zoom-in-up" data-aos-duration="1000" className="card-container" id={job.projectType} accessKey={job.userId}>
+            <div data-aos="zoom-in-up" data-aos-duration="1000" className="card-container" id={job.projectType} >
             <Link to={`/post/${job._id}`} key={job._id}> 
             <div className="job">
                 Name of person: {job.name}
@@ -73,7 +77,7 @@ export default function Card(props) {
             <div className="bid">
               <Bid job={job}/>
             </div>
-            <div className="button-div">
+            <div className="button-div" accessKey={job.userId}>
                 <button id='editButton'><Link to={`/posts/edit/${job._id}`}>EDIT</Link></button>
                 <button value={job._id} onClick={handleSubmit} id='dltButton'>DELETE</button>
             </div>
