c6385d7e8d4f22c33ac7273e4093a41ceec87ea4
Gabriel Deleon <gabrieldeleon@gabriels-mbp.myfiosgateway.com>
2021-10-07 13:03:53 -0400

    removing accesskey manipulation as it effects screenreader accesibility

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -41,8 +41,8 @@ export default function Card(props) {
     
   const handleSubmit = async (e) => {
     console.log(props.user.id)
-    console.log(e.target.parentElement.accessKey)
-    if (props.user.id === e.target.parentElement.accessKey) {
+    console.log(e.target.parentElement.id)
+    if (props.user.id === e.target.parentElement.id) {
       if (window.confirm("Are you sure you want to delete the job post?")) { 
       handleDelete(e)
       history.push("/")
@@ -50,49 +50,10 @@ export default function Card(props) {
     }
         }
     }
-  
-  // const handleUp = (e) => {
-  //     if (count > copyData.length) {
-  //       count = 0;
-  //       page = 0;
-  //     } else {
-  //       countSplice(copyData, count, count + 10, page, "up")
-  //     }
-  // }
-  
-  // const handleDown = (e) => {
-  //     if (count < 0) {
-  //       count = 0;
-  //       page = 0;
-  //     } else {
-  //       countSplice(copyData, count, count + 10, page, "down")
-  //     }
-  //   }
-
-  //   function countSplice(array, begin, end, page, direction) {
-  //     console.log(count)
-    
-  //     function helper(array, begin, end){
-  //       let newArr = array.slice(begin, end)
-  //       console.log(newArr)
-  //     }  
-  //     if (direction === "up") {
-  //       helper(array, begin, end)
-  //       count += 10;
-  //       page += 1;
-        
-  //     } else if (direction === "down") {
-  //       helper(array, begin - 20, end - 20)
-  //       count -= 10;
-  //       page -= 1;
-  //     }
-  //   }
 
     return (
         
       <>
-        {/* <button className="down" onClick={handleDown} > ← </button>
-        <button className="up" onClick={handleUp} > → </button> */}
         {jobs.map((job) => (
           <div data-aos="zoom-in-up" data-aos-duration="1000" className="card-container"
             id={job.projectType} key={job.name}>
@@ -116,7 +77,7 @@ export default function Card(props) {
             <div className="card-bid">
               <Bid job={job}/>
             </div>
-            <div className="button-div" accessKey={job.userId}>
+            <div className="button-div" id={job.userId}>
               <button id='editButton'><Link to={`/posts/edit/${job._id}`}>EDIT</Link></button>
                 <button value={job._id} onClick={handleSubmit} id='dltButton'>DELETE</button>
             </div>
